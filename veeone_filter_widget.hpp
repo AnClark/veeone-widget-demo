034ac7acf7f40782865ffc28724a28d31a228776
AnClark <clarklaw4701@qq.com>
2024-11-01 11:11:23 +0800

    (To be rebased) Main codebase

---
+++ veeone_filter_widget.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <imgui.h>
+
+#include <cstdint>
+
+namespace VeeOneWidgets
+{
+
+/**
+ * Enum: DCF Type
+ */
+enum class DcfType
+{
+    t_LPF = 0,
+    t_BPF,
+    t_HPF,
+    t_BRF
+};
+
+/*
+ * Enum: Filter slope
+ */
+enum class FilterSlope
+{
+    s_12dbPerOct = 0, // 12dB/oct
+    s_24dBPerOct,     // 24dB/oct
+    s_biquad,
+    s_formant
+};
+
+void DCFCurve(const char *id, float *cutoff, float *resonance, int *type, int *slope, uint32_t width, uint32_t height);
+
+void _drawFormantCurve(ImVec2 &pos, float *cutoff, float *resonance, int *type, int *slope, uint32_t width,
+                       uint32_t height);
+
+} // namespace VeeOneWidgets
